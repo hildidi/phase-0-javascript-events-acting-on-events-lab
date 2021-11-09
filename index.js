11924ac16606f3050f27e6b7507d06e3d10d6ee9
Hilda Diana Cardenas <hcardenas00@gmail.com>
2021-11-09 01:07:14 -0500

    commit tree f446a40c28d783058df971c2db5d6f74fa956157

--- index.js
+++ index.js
@@ -1,33 +1,52 @@
-// moveDodgerLeft()
+// // moveDodgerLeft()
+// function moveDodgerLeft() {
+//     const leftNumbers = dodger.style.left.replace("px", "");
+//     const left = parseInt(leftNumbers, 10);
+  
+//     if (left > 0) {
+//       dodger.style.left = `${left - 1}px`;
+//     }
+//   }
+
+// document.addEventListener("keydown", function (event) {
+//     if (event.key === "ArrowLeft") {
+//       moveDodgerLeft();
+//     }
+//   });
+  
+  
+
+//   // moveDodgerRight()
+//   function moveDodgerRight() {
+//     const leftNumbers = dodger.style.left.replace("px", "");
+//     const left = parseInt(leftNumbers, 10);
+  
+//     if (left < 360) {
+//       dodger.style.left = `${left + 1}px`;
+//     }
+//   }
+
+//   document.addEventListener("keydown", function (event) {
+//     if (event.key === "ArrowRight") {
+//     moveDodgerRight();
+//     }
+// });
+
 function moveDodgerLeft() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
-  
-    if (left > 0) {
-      dodger.style.left = `${left - 1}px`;
-    }
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
   }
+}
 
-document.addEventListener("keydown", function (event) {
-    if (event.key === "ArrowLeft") {
-      moveDodgerLeft();
-    }
-  });
-  
-  
+// function moveDodgerLeft(){
 
-  // moveDodgerRight()
-  function moveDodgerRight() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
-  
-    if (left < 360) {
-      dodger.style.left = `${left + 1}px`;
-    }
-  }
+// }
 
-  document.addEventListener("keydown", function (event) {
-    if (event.key === "ArrowRight") {
-    moveDodgerRight();
-    }
-});
+
+
+// function moveDodgerRight(){
+
+// }
